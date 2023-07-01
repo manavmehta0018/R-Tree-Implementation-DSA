bdfbd6fa08f1b8a5fa7108296d2884ac25fd7b9c
manavmehta0018 <98983003+manavmehta0018@users.noreply.github.com>
2023-07-01 21:51:28 +0530

    Add files via upload

---
+++ R-Tree-Implementation-master/User.java
@@ -0,0 +1,14 @@
+package AudiTicketBook;
+
+public class User {
+    protected String password;
+    protected String userid;
+
+
+    public User(String userid, String password) {
+        this.password = password;
+        this.userid = userid;
+    }
+
+
+}
